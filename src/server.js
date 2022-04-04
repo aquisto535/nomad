1ada22f8b54d9d68827f7aa56871e690c5f1cc8e
Jung Tae Min <slrkaksemfdj@naver.com>
2022-04-04 11:50:14 +0900

    going2
    gogogo

--- src/server.js
+++ src/server.js
@@ -1,28 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
-const logger = morgan("dev")
-
-const handleHome = (req, res, next) =>{
- return res.send("I stil love you")
-}
-
-const login = (req, res) => {
-  return res.send("login");
-};
-
-app.use(logger)
-
-
-app.get("/", handleHome);
-app.get("/login", login)
-
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-
 app.listen(PORT, handleListening);
