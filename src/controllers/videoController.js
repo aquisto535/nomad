2a2f08c26241a2d25a547fcd4de0108fbceea69a
Jung Tae Min <slrkaksemfdj@naver.com>
2022-04-11 15:03:58 +0900

    pug conditional
    yet long way to go

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,14 @@
-export const trending = (req, res) =>  res.render("home", {pageTitle: "Home"})
-export const see = (req, res) =>  res.render("watch")
-export const edit = (req, res) => res.render("edit")
+const fakeUser = {
+  username: "Nicolas",
+  loggedIn: true,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-  return res.send("Delete Video");};
+  return res.send("Delete Video");
+};
