ab9b4e868d1adc2a20a3597c32a082c03fb7ba93
Jung Tae Min <slrkaksemfdj@naver.com>
2022-04-16 01:46:05 +0900

    database start!
    here we go!

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,48 @@
+let videos = [
+  {
+    title: "First video",
+    rating: 5,
+    comment: 2,
+    createdAt: "2minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+]
+
+
 export const trending = (req, res) =>{
-  const videos = [
-    {
-      title: "Hello",
-      rating: 5,
-      comment: 2,
-      createdAt: "2minutes ago",
-      views:"59",
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ]
   res.render("home", { pageTitle: "Home", videos });
 }
+export const watch = (req, res) => {
+  const {id} = req.params
+  const video = videos[id-1]
+  return res.render("watch", {pageTitle:`watching ${video.title}`, video});
+}
+export const getEdit = (req, res) =>{
+  const {id} = req.params
+  const video = videos[id-1]
+  res.render("edit", {pageTitle:`Editing ${video.title}`, video});
+}
+export const postEdit = (req, res) =>{
+  const {id} = req.params
+  const {title} = req.body;
+  videos[id-1].title = title
+  return res.redirect(`/videos/${id}`)
+}
 
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  return res.send("Delete Video");
-};
--- src/server.js
+++ src/server.js
@@ -10,8 +10,9 @@ const app = express();
 const logger = morgan("dev");
 
 app.set("view engine", "pug");
-app.set("views", "./src/views")
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true}))
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
