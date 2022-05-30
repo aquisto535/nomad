d4da0afcc930cbcc7714f4d9e47ac4601231db12
Jung Tae Min <slrkaksemfdj@naver.com>
2022-05-30 21:33:07 +0900

    USER AUTHENTICATION Day5
    until the end~

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ["airbnb-base"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,41 +1,45 @@
-import User from "../models/User"
+import User from "../models/User";
 import bcrypt from "bcrypt";
+import fetch from "node-fetch";
 
-export const getjoin = (req, res) => res.render("join", {pageTitle:"Join" });
+export const getjoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postjoin = async (req, res) => {
-   
-    const { name , username , email , password, password2, location } = req.body
-    
-    if(password !== password2){
-      return res.status(400).render("join", {pageTitle:"Join", 
-      errorMessage:"Password confirmation does not match." 
-    })}
+  const { name, username, email, password, password2, location } = req.body;
 
-    const Exits = await User.exists({$or: [{username}, {email}]});
-    if(Exits){
-      return res.status(400).render("join", {pageTitle:"Join", 
-      errorMessage:"This username/e-mail is already taken." 
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "Password confirmation does not match.",
     });
-    }
+  }
 
-    try {
-      await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
-      });
-      return res.redirect("/login");
-    } catch (error) {
-      return res.status(400).render("join", {
-        pageTitle: "Upload Video",
-        errorMessage: error._message,
-      });
-    }
-  };
+  const Exits = await User.exists({ $or: [{ username }, { email }] });
+  if (Exits) {
+    return res.status(400).render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/e-mail is already taken.",
+    });
+  }
 
-export const getlogin = (req, res) => res.render("login", {pageTitle:"Login"})
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const getlogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
@@ -56,12 +60,55 @@ export const postLogin = async (req, res) => {
   }
 
   req.session.loggedIn = true;
-  req.session.user = user 
+  req.session.user = user;
   return res.redirect("/");
 };
+
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SCERET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json();
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const userRequest = await (
+      await fetch("https://api.github.com/user", {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    console.log(userRequest);
+  } else {
+    return res.redirect("/login");
+  }
+};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("logout")
-export const see = (req, res) => res.send("See User")
-
-
+export const logout = (req, res) => res.send("logout");
+export const see = (req, res) => res.send("See User");
