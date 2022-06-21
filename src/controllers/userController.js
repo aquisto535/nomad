98541e69d687d833d7a20a5818ca1310183a3473
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-21 23:54:28 +0900

    USER PROFILE Day1
    Never give up!

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,9 @@
 import User from "../models/User";
 import bcrypt from "bcrypt";
 import fetch from "node-fetch";
-import { redirect } from "express/lib/response";
 
 export const getjoin = (req, res) => res.render("join", { pageTitle: "Join" });
+
 export const postjoin = async (req, res) => {
   const { name, username, email, password, password2, location } = req.body;
 
@@ -142,9 +142,55 @@ export const finishGithubLogin = async (req, res) => {
   }
 };
 
-export const edit = (req, res) => res.send("Edit User");
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
+
+export const getEdit = (req, res) => {
+  res.render("edit-profile", { pageTitle: "Edit-profile" });
+};
+
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id, avatarUrl, email: sessionEmail, username: sessionUsername },
+    },
+    body: { name, email, username, location },
+    file,
+  } = req;
+  let searchParam = [];
+  if (sessionEmail !== email) {
+    searchParam.push({ email });
+  }
+  if (sessionUsername !== username) {
+    searchParam.push({ username });
+  }
+  if (searchParam.length > 0) {
+    const foundUser = await User.findOne({ $or: searchParam });
+    if (foundUser && foundUser._id.toString() !== _id) {
+      return res.status(HTTP_BAD_REQUEST).render("edit-profile", {
+        pageTitle: "Edit Profile",
+        errorMessage: "This username/email is already taken.",
+      });
+    }
+  }
+  const isHeroku = process.env.NODE_ENV === "production";
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
+      name,
+      email,
+      username,
+      location,
+    },
+    {
+      new: true,
+    }
+  );
+  req.session.user = updatedUser;
+  return res.redirect("/users/edit");
+};
+
 export const see = (req, res) => res.send("See User");
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,13 +1,22 @@
 import express from "express";
-import { getjoin,postjoin, getlogin, postLogin} from "../controllers/userController";
-import {  search, home } from "../controllers/videoController";
+import {
+  getjoin,
+  postjoin,
+  getlogin,
+  postLogin,
+} from "../controllers/userController";
+import { search, home } from "../controllers/videoController";
+import { publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getjoin).post(postjoin);
-rootRouter.route("/login").get(getlogin).post(postLogin)
-rootRouter.get("/search", search)
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getjoin).post(postjoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getlogin)
+  .post(postLogin);
+rootRouter.get("/search", search);
 
-
-export default rootRouter;    
+export default rootRouter;
