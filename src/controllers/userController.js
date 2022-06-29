2030f87252b32169a3ef8ef643b6ee84aca5a73f
Jung Tae Min <slrkaksemfdj@naver.com>
2022-06-29 18:34:42 +0900

    STYLES

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -238,7 +238,13 @@ export const postChangepassword = async (req, res, next) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
   // populate makes it possible to Add videos array to user.and it uploads everything of videos.
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
