1c43d64ae4b4137b88542ce4514965b8e98d969b
Tae Min Jung <71391013+aquisto535@users.noreply.github.com>
2022-07-29 03:58:04 +0900

    DEPLOYMENT Day 1
    it's final step!!

--- src/init.js
+++ src/init.js
@@ -1,13 +1,14 @@
+import "regenerator-runtime";
 import "dotenv/config";
 import "./db";
 import "./models/video";
 import "./models/User";
-import app from "./server"
+import "./models/Comment";
+import app from "./server";
 
 const PORT = 5000;
 
-
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-  
+
 app.listen(PORT, handleListening);
